17da377b3d0c7e8995883b7115fefa559f1aafea
aryo <aryofreeyoung@gmail.com>
2019-12-31 09:48:30 +0700

    belajar package oop

---
+++ src/p1/Test.java
@@ -0,0 +1,7 @@
+package p1;
+
+public class Test{
+	public void tampilTeks(){
+		System.out.println("Tampil teks dari P1");
+	}
+}
---
+++ src/p2/Test.java
@@ -0,0 +1,7 @@
+package p2;
+
+public class Test{
+	public void tampilTeks(){
+		System.out.println("Tampil teks dari P2");
+	}
+}
